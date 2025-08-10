b65a5879d8a800680572cac85b59ea36dc7c6b1d
welkambabreu <welkambabreu@gmail.com>
2025-08-10 19:50:22 -0300

    adição do arquivo autenticação e transferências

---
+++ helpers/autenticacao.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+const postLogin = JSON.parse(open('../fixtures/postLogin.json'));
+export function obterToken() {
+
+    const url = 'http://localhost:3000/login';
+
+    const payload = JSON.stringify(postLogin);
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const res = http.post(url, payload, params);
+
+    return res.json('token')
+}
---
+++ tests/transferencias.test.js
@@ -0,0 +1,38 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+import { obterToken } from '../helpers/autenticacao.js';
+
+export const options = {
+  iterations: 1
+};
+
+export default function () {
+
+  const token = obterToken(); // aqui ocorre o reaproveitamento do código
+
+  const url = 'http://localhost:3000/transferencias';
+
+  const payload = JSON.stringify(
+    {
+      "contaOrigem": 1,
+      "contaDestino": 2,
+      "valor": 11,
+      "token": ""
+    }
+
+  );
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token
+    },
+  };
+
+
+  let res = http.post(url, payload, params);
+
+  check(res, { "status is 201": (res) => res.status === 201 });
+
+  sleep(1);
+}
